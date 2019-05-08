3dc7c0c9192c204406a8ff7552d48ad220f3b0c1
Vivify Ideas <vivify@Vivifys-MacBook-Pro.local>
2019-05-08 13:52:47 +0200

    JS Prototypes and Classes

---
+++ jsPrototypesAndClasses.js
@@ -0,0 +1,90 @@
+class Character {
+  static numberOfCharacters = 0;
+  static showNumberOfCharacters = () => console.log(this.numberOfCharacters);
+
+  constructor() {
+    if (new.target === Character) {
+      throw new Error('Cannot create instance of abstract class')
+    }
+    this.x = Math.round(Math.random() * 10);
+    this.y = Math.round(Math.random() * 10);
+    Character.numberOfCharacters++;
+  }
+
+  showPosition() {
+    console.log('X: ' + this.x + ' Y: ' + this.y);
+  }
+
+  setCoordinates(x, y) {
+    if (x > 10 || y > 10) {
+      throw new Error('Coordinates out of boundaries')
+    }
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class PlayerCharacter extends Character {
+  constructor() {
+    super()
+  }
+}
+
+class NonPlayerCharacter extends Character {
+  constructor() {
+    super()
+  }
+}
+
+const char1 = new PlayerCharacter();
+char1.setCoordinates(5, 6);
+const char2 = new NonPlayerCharacter();
+char1.showPosition();
+char2.showPosition();
+Character.showNumberOfCharacters();
+
+
+// Prototypes
+
+const Character2 = function () {
+  if (new.target === Character) {
+    throw new Error('Cannot create instance of abstract class')
+  }
+}
+
+Character2.numberOfCharacters = 0;
+Character2.showNumberOfCharacters = function () {
+  console.log(this.numberOfCharacters)
+}
+
+Character2.prototype = {
+  x: Math.round(Math.random() * 10),
+  y: Math.round(Math.random() * 10),
+  showPosition: function () {
+    console.log('X: ' + this.x + ' Y: ' + this.y);
+  },
+  setCoordinates: function (x, y) {
+    if (x > 10 || y > 10) {
+      throw new Error('Coordinates out of boundaries')
+    }
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const PlayerCharacter2 = function () {
+  Character2.numberOfCharacters++
+}
+PlayerCharacter2.prototype = Character2.prototype
+
+const NonPlayerCharacter2 = function () {
+  Character2.numberOfCharacters++
+}
+NonPlayerCharacter2.prototype = Character2.prototype;
+
+const char3 = new PlayerCharacter2();
+char3.setCoordinates(5, 6);
+const char4 = new NonPlayerCharacter2();
+char3.showPosition();
+char4.showPosition();
+Character2.showNumberOfCharacters();
